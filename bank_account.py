fed36f5d39c20b1026dca1599527391b135da5a5
Anhad Singh Gujral <anhadsinghgujral@gmail.com>
2025-09-22 18:41:50 -0400

    Made getter method for balance

--- bank_account.py
+++ bank_account.py
@@ -3,4 +3,8 @@ class BankAccount:
     def __init__(self, name, balance):
         self.name = name
         self.balance = balance
-        print(f"Account created \n Name : {name} \n Balance : {balance}")
+        print(f"Account created \n Name : {name} \n Balance : ${balance}")
+
+    def getBalance(self):
+        print(f"Account balance is ${self.balance:.2f}")
+        return self.balance
